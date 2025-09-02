fd839725f025c6aaa0ab973d0b90e26f3738b8ef
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-02 18:23:45 +0100

    Add word-letter section / component, autofill the letters with word letters, add styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import './App.css'
 import Header from './components/Header'
 import Status from './components/Status'
 import Chips from './components/Chips'
+import Word from './components/Word'
 import {languages} from '../languages'
 import { nanoid } from 'nanoid'
 
 export default function App() {
 
-  const chipElements = languages.map((language) => <Chips key={nanoid()} name={language.name} bgColor={language.backgroundColor} color={language.color}/>)
+  const chipElements = languages.map((language) => <Chips key={language.name} name={language.name} bgColor={language.backgroundColor} color={language.color}/>)
+
+  const [ currentWord, setCurrentWord ] = useState("react")
+
+  const letterArray = currentWord.split("")
+  const letterElements = letterArray.map(letter =>
+      <Word letter={letter}/>
+    )
 
   return (
     <>
@@ -17,6 +25,9 @@ export default function App() {
         <Status />
         <section className="chips-container">
           {chipElements}
+        </section>
+        <section className="letter-section">
+          {letterElements}
         </section>
       </main>
     </>
---
+++ src/components/Word.jsx
@@ -0,0 +1,5 @@
+export default function Word({letter}) {
+    return (
+        <span className="letter">{letter}</span>
+    )
+}
